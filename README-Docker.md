e1ebe09ced41087d9eef2dcd2f01207a3d5c8bba
ripenedcat <550107737@qq.com>
2025-11-17 15:11:16 +0800

    bypass docker package lint check

---
+++ README-Docker.md
@@ -0,0 +1,5 @@
+docker 构建：
+
+docker build -f Dockerfile.database -t lucaslifes/lucasprivatehub:lobechat .
+
+docker build -f Dockerfile.database -t lucaslifes/lucasprivatehub:lobechat --no-cache .
--- src/config/featureFlags/schema.ts
+++ src/config/featureFlags/schema.ts
@@ -84,20 +84,20 @@ export const DEFAULT_FEATURE_FLAGS: IFeatureFlags = {
 
   plugins: true,
   dalle: true,
-  ai_image: true,
+  ai_image: false,
 
   check_updates: true,
   welcome_suggest: true,
   token_counter: true,
 
-  knowledge_base: true,
+  knowledge_base: false,
   rag_eval: false,
 
   clerk_sign_up: true,
 
   cloud_promotion: false,
 
-  market: true,
+  market: false,
   speech_to_text: true,
   changelog: true,
 
@@ -106,7 +106,7 @@ export const DEFAULT_FEATURE_FLAGS: IFeatureFlags = {
   // the flags below can only be used with commercial license
   // if you want to use it in the commercial usage
   // please contact us for more information: hello@lobehub.com
-  commercial_hide_github: false,
+  commercial_hide_github: true,
   commercial_hide_docs: false,
 };
 
